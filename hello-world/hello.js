1f9c04e3d1847b9fc4982994b26e5880d34f67ec
Caleb Smith <smithcaleb042@gmail.com>
2021-03-04 09:05:33 -0700

    added more pages and static assets

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -4,8 +4,26 @@ const app = express();
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
+  res.redirect("/english");
+});
+
+app.get("/english", (req, res) => {
   res.render("hello-world-english");
+});
+
+app.get("/french", (req, res) => {
+  res.render("hello-world-french");
+});
+
+app.get("/serbian", (req, res) => {
+  res.render("hello-world-serbian");
+});
+
+app.get("/spanish", (req, res) => {
+  res.render("hello-world-spanish");
 });
 
 app.listen(3001, "localhost", () => {
