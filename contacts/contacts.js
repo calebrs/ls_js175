ed5eaa712822bed624c4700811b74b47a8f891dd
Caleb Smith <smithcaleb042@gmail.com>
2021-03-05 11:53:14 -0700

    added error handling to the user inputs

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -62,10 +62,85 @@ app.get("/contacts/new", (req, res) => {
   res.render("new-contact");
 });
 
-app.post("/contacts/new", (req, res) => {
-  contactData.push({ ...req.body });
-  res.redirect("/contacts");
-});
+app.post("/contacts/new",
+  (req, res, next) => {
+    res.locals.errorMessages = [];
+    next();
+  },
+  (req, res, next) => {
+    res.locals.firstName = req.body.firstName.trim();
+    res.locals.lastName = req.body.lastName.trim();
+    res.locals.phoneNumber = req.body.phoneNumber.trim();
+    next();
+  },
+  (req, res, next) => {
+    let firstName = res.locals.firstName;
+    if (firstName.length === 0) {
+      res.locals.errorMessages.push("First name is required");
+    } else if (firstName.length > 25) {
+      res.locals.errorMessages.push("First name is too long. Maximum length is 25 characters");
+    } else if (!firstName.match(/^[a-zA-Z]+$/)) {
+      res.locals.errorMessages.push("First name contains invalid characters. Must be alphabetic.");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    let lastName = res.locals.lastName;
+    if (lastName.length === 0) {
+      res.locals.errorMessages.push("Last name is required");
+    } else if (lastName.length > 25) {
+      res.locals.errorMessages.push("Last name is too long. Maximum length is 25 characters");
+    } else if (!lastName.match(/^[a-zA-Z]+$/)) {
+      res.locals.errorMessages.push("Last name contains invalid characters. Must be alphabetic.");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    let phoneNumber = res.locals.phoneNumber;
+    if (phoneNumber.length === 0) {
+      res.locals.errorMessages.push("Phone number is required");
+    } else if (!phoneNumber.match(/[0-9]{3}-[0-9]{3}-[0-9]{4}/)) {
+      res.locals.errorMessages.push("Phone number must match format: ###-###-####");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    let fullName = `${res.locals.firstName} ${res.locals.lastName}`;
+    let foundContact = contactData.find(contact => {
+      return `${contact.firstName} ${contact.lastName}` === fullName;
+    });
+
+    if (foundContact) {
+      res.locals.errorMessages.push(`${fullName.lastName} is already on you contact list.`);
+    }
+
+    next()
+  },
+  (req, res, next) => {
+    if (res.locals.errorMessages.length > 0) {
+      res.render("new-contact", {
+        errorMessages: res.locals.errorMessages,
+        firstName: res.locals.firstName,
+        lastName: res.locals.lastName,
+        phoneNumber: res.locals.phoneNumber,
+      });
+    } else {
+      next();
+    }
+  },
+  (req, res) => {
+    contactData.push({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phoneNumber: req.body.phoneNumber,
+    });
+
+    res.redirect("/contacts");
+  }
+);
 
 app.listen(3000, "localhost", () => {
   console.log("Listening to port 3000.");
